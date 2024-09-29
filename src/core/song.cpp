68b8bd7a355850d5cea27b0ca1b157bf9b1246d9
Jonas Kvinge <jonas@jkvinge.net>
2024-09-29 03:24:44 +0200

    Song: Add init from file setter

--- src/core/song.cpp
+++ src/core/song.cpp
@@ -567,6 +567,8 @@ void Song::set_musicbrainz_work_id(const QString &v) { d->musicbrainz_work_id_ =
 void Song::set_ebur128_integrated_loudness_lufs(const std::optional<double> v) { d->ebur128_integrated_loudness_lufs_ = v; }
 void Song::set_ebur128_loudness_range_lu(const std::optional<double> v) { d->ebur128_loudness_range_lu_ = v; }
 
+void Song::set_init_from_file(const bool v) { d->init_from_file_ = v; }
+
 void Song::set_stream_url(const QUrl &v) { d->stream_url_ = v; }
 
 const QUrl &Song::effective_stream_url() const { return !d->stream_url_.isEmpty() && d->stream_url_.isValid() ? d->stream_url_ : d->url_; }
--- src/core/song.h
+++ src/core/song.h
@@ -315,6 +315,8 @@ class Song {
   void set_ebur128_integrated_loudness_lufs(const std::optional<double> v);
   void set_ebur128_loudness_range_lu(const std::optional<double> v);
 
+  void set_init_from_file(const bool v);
+
   void set_stream_url(const QUrl &v);
 
   const QUrl &effective_stream_url() const;
