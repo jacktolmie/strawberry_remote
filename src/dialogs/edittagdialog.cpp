ea98086167e69138b7276c91bf77b952497cc6c9
Jonas Kvinge <jonas@jkvinge.net>
2024-10-19 21:53:22 +0200

    EditTagDialog: Move hint texts to class

--- src/dialogs/edittagdialog.cpp
+++ src/dialogs/edittagdialog.cpp
@@ -102,11 +102,12 @@
 #include "tagreadermessages.pb.h"
 
 namespace {
-constexpr char kTagsDifferentHintText[] = QT_TR_NOOP("(different across multiple songs)");
-constexpr char kArtDifferentHintText[] = QT_TR_NOOP("Different art across multiple songs.");
 constexpr char kSettingsGroup[] = "EditTagDialog";
 constexpr int kSmallImageSize = 128;
 }  // namespace
+
+const char EditTagDialog::kTagsDifferentHintText[] = QT_TR_NOOP("(different across multiple songs)");
+const char EditTagDialog::kArtDifferentHintText[] = QT_TR_NOOP("Different art across multiple songs.");
 
 EditTagDialog::EditTagDialog(Application *app, QWidget *parent)
     : QDialog(parent),
