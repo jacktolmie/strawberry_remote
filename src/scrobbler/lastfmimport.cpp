d5c3f6fdeb9d2b92d07c6ee2211740c5283f513d
Jonas Kvinge <jonas@jkvinge.net>
2025-04-08 23:05:44 +0200

    LastFMImport: Fix progress

--- src/scrobbler/lastfmimport.cpp
+++ src/scrobbler/lastfmimport.cpp
@@ -228,7 +228,7 @@ void LastFMImport::ImportData(const bool lastplayed, const bool playcount) {
 
 void LastFMImport::FlushRequests() {
 
-  if (!recent_tracks_requests_.isEmpty()) {
+  if (!recent_tracks_requests_.isEmpty() && (!playcount_ || (playcount_total_ > 0 || top_tracks_requests_.isEmpty()))) {
     SendGetRecentTracksRequest(recent_tracks_requests_.dequeue());
     return;
   }
@@ -564,8 +564,7 @@ void LastFMImport::GetTopTracksRequestFinished(QNetworkReply *reply, const int p
 
 void LastFMImport::UpdateTotalCheck() {
 
-  if ((!playcount_ || playcount_total_ > 0) && (!lastplayed_ || lastplayed_total_ > 0))
-    Q_EMIT UpdateTotal(lastplayed_total_, playcount_total_);
+  Q_EMIT UpdateTotal(lastplayed_total_, playcount_total_);
 
 }
 
