6536acf24fdc440436515f24c8f92aad5235fba9
Jonas Kvinge <jonas@jkvinge.net>
2025-06-09 04:12:23 +0200

    DeviceManager: Remove device refresh

--- src/device/devicemanager.cpp
+++ src/device/devicemanager.cpp
@@ -290,7 +290,6 @@ QVariant DeviceManager::data(const QModelIndex &idx, int role) const {
       if (info->size_ > 0) {
         text = text + QStringLiteral(" (%1)").arg(Utilities::PrettySize(info->size_));
       }
-      if (info->device_) info->device_->Refresh();
       return text;
     }
 
